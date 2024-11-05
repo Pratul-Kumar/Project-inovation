416ddee340cd8814b07ca99ec2ce6dcf44ba0dd8
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2024-11-05 14:35:28 +0530

    member faculty and gallary route created

--- app.js
+++ app.js
@@ -15,8 +15,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route
+
+//home route
 app.get('/', (req, res) => {
-  res.render('student council.ejs', { title: 'Home Page' });
+  res.render('student council', { title: 'Home Page' });
+});
+
+//member routes
+app.get('/members', (req, res) => {
+  res.render('OurTeam', { title: 'Member Page' });
+});
+
+//facauty routes
+
+app.get('/faculty', (req, res) => {
+  res.render('FaculityPage', { title: 'Faculty Page' });
+});
+
+//gallary routes
+
+app.get('/gallery', (req, res) => {
+  res.render('Gallery', { title: 'Gallery Page' });
 });
 
 // Start the server
